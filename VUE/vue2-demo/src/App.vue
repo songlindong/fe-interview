ec16bca6fae5914bff8d21c4e88314d04e7ce795
songlindong <857344690@qq.com>
2023-07-10 00:00:41 +0800

    feat: vue相关题目

--- VUE/vue2-demo/src/App.vue
+++ VUE/vue2-demo/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div ref="text">{{ a }}, {{ b }}</div>
   </div>
 </template>
 
@@ -12,6 +13,21 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      a: 1,
+      b: 2,
+    }
+  },
+  created() {
+    setTimeout(() => {
+      this.a = 3,
+      this.$nextTick(() =>{
+        console.log(this.$ref.text.innerHTML)
+      })
+      this.b = 4
+    }, 1000)
   }
 }
 </script>
