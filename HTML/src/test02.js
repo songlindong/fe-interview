2c377c371ea2d2cdafb0e50797ed755ff974fecd
songlindong <857344690@qq.com>
2023-07-24 13:57:36 +0800

    feat: 删除无用代码

--- HTML/src/test02.js
+++ HTML/src/test02.js
@@ -26,14 +26,3 @@ let arr1 =  [1, 2, 3, 4]
 const arr = new quene()
   arr.push(5)
   arr.pop()
-
-const num = {
-  a: 10,
-  add: function() {
-    return this.a + 2
-  },
-  reduce: () => this.a - 2
-}
-
-console.log(num.add())
-console.log(num.reduce())
