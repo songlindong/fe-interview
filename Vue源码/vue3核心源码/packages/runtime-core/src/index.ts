08a0f1a90f238c6a93f685e514a08c27abd0db50
songlindong <857344690@qq.com>
2023-07-26 17:34:49 +0800

    feat: vue3核心源码

---
+++ Vue源码/vue3核心源码/packages/runtime-core/src/index.ts
@@ -0,0 +1,27 @@
+export * from "./h";
+export * from "./createApp";
+export { getCurrentInstance, registerRuntimeCompiler } from "./component";
+export { inject, provide } from "./apiInject";
+export { renderSlot } from "./helpers/renderSlot";
+export { createTextVNode, createElementVNode } from "./vnode";
+export { createRenderer } from "./renderer";
+export { toDisplayString } from "@mini-vue/shared";
+export {
+  // core
+  reactive,
+  ref,
+  readonly,
+  // utilities
+  unRef,
+  proxyRefs,
+  isReadonly,
+  isReactive,
+  isProxy,
+  isRef,
+  // advanced
+  shallowReadonly,
+  // effect
+  effect,
+  stop,
+  computed,
+} from "@mini-vue/reactivity";
---
+++ Vue源码/vue3核心源码/readme.md
@@ -0,0 +1,3 @@
+# Vue3 核心源码
+
+1. init、update、 function call执行的逻辑
