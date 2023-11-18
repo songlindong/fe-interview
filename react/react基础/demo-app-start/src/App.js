5c884f0b94abb4bc823afc6223ba7980c5d97c0d
songlindong <857344690@qq.com>
2023-11-18 17:34:16 +0800

    feat: 组件

--- react/react基础/demo-app-start/src/App.js
+++ react/react基础/demo-app-start/src/App.js
@@ -2,8 +2,27 @@
 import './App.css';
 import {useState} from 'react';
 
-// 函数组件
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!')
+  }
+  return (
+    <button onClick={handleClick}>
+      I'm a button
+    </button>
+  )
+}
+const products = [
+  { title: 'Cabbage', id: 1 },
+  { title: 'Garlic', id: 2 },
+  { title: 'Apple', id: 3 },
+];
 
+const listItems = products.map(product =>
+  <li key={product.id}>
+     {product.title}
+  </li>
+);
 function App() {
   // state,dispatch
   // 1. 状态，我要用这个值，去触发界面更新
@@ -19,6 +38,10 @@ function App() {
       <div>this title is{title}</div>
       <div>this data is{data}</div>
       <button onClick={handleClick}>change</button>
+      <MyButton />
+      <ul>
+        {listItems}
+      </ul>
     </div>
   );
 }
